2db50dfbe469728c2731d7603df3f75c609f158c
Alexander Baklushin <din.myrk@gmail.com>
2022-10-17 11:55:43 +0300

    первый коммит

---
+++ src/main/java/com/example/streamapianaoptional/StreamApIanaOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamapianaoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApIanaOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApIanaOptionalApplication.class, args);
+    }
+
+}
